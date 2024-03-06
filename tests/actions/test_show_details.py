5c9029ff7214a7952f5aad61683472a0ddac98d0
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-03-06 03:50:22 -0300

    test: function show_details refac

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -1,19 +1,50 @@
+import pytest
 from datetime import date
 import os
 from pro_filer.actions.main_actions import show_details  # NOQA
 
 
-def test_show_details_existing_file(capsys, tmp_path):
-    file = tmp_path / "file.txt"
-    file.touch()
-    file.write_text("Hello, world!")
+@pytest.fixture
+def existing_file(tmp_path):
+    file_path = tmp_path / "file.txt"
+    file_path.touch()
+    file_path.write_text("Hello, world!")
+    return str(file_path)
 
-    last_modified = date.fromtimestamp(os.path.getmtime(str(file)))
-    size = os.path.getsize(str(file))
-    file_name = str(file).split("/")[-1]
-    _, file_extension = os.path.splitext(file_name)
 
-    context = {"base_path": str(file)}
+@pytest.fixture
+def non_existing_file():
+    return "some/non/existing/file.txt"
+
+
+@pytest.fixture
+def file_with_no_extension(tmp_path):
+    file_path = tmp_path / "file"
+    file_path.touch()
+    return str(file_path)
+
+
+@pytest.fixture
+def get_file_details():
+    def _get_file_details(file_path):
+        last_modified = date.fromtimestamp(os.path.getmtime(str(file_path)))
+        size = os.path.getsize(str(file_path))
+        file_name = str(file_path).split("/")[-1]
+        _, file_extension = os.path.splitext(file_name)
+
+        return file_name, size, last_modified, file_extension
+
+    return _get_file_details
+
+
+def test_show_details_existing_file(capsys, existing_file, get_file_details):
+    file_name, size, last_modified, file_extension = get_file_details(
+        existing_file
+    )
+
+    context = {"base_path": existing_file}
+
+    file_type = "directory" if os.path.isdir(existing_file) else "file"
 
     show_details(context)
 
@@ -21,7 +52,7 @@ def test_show_details_existing_file(capsys, tmp_path):
     expected_output = (
         f"File name: {file_name}\n"
         f"File size in bytes: {size}\n"
-        f"File type: {'directory' if os.path.isdir(str(file)) else 'file'}\n"
+        f"File type: {file_type}\n"
         f"File extension: {file_extension or '[no extension]'}\n"
         f"Last modified date: {last_modified}\n"
     )
@@ -29,10 +60,8 @@ def test_show_details_existing_file(capsys, tmp_path):
     assert captured.err == ""
 
 
-def test_show_details_non_existing_file(capsys):
-    file = "some/non/existing/file.txt"
-
-    context = {"base_path": file}
+def test_show_details_non_existing_file(capsys, non_existing_file):
+    context = {"base_path": non_existing_file}
 
     show_details(context)
 
@@ -40,24 +69,25 @@ def test_show_details_non_existing_file(capsys):
     assert captured.out == "File 'file.txt' does not exist\n"
     assert captured.err == ""
 
-def test_show_details_file_with_no_extension(capsys, tmp_path):
-    file = tmp_path / "file"
-    file.touch()
 
-    last_modified = date.fromtimestamp(os.path.getmtime(str(file)))
-    size = os.path.getsize(str(file))
-    file_name = str(file).split("/")[-1]
-    _, file_extension = os.path.splitext(file_name)
+def test_show_details_file_with_no_extension(
+    capsys, file_with_no_extension, get_file_details
+):
+    file_name, size, last_modified, file_extension = get_file_details(
+        file_with_no_extension
+    )
 
-    context = {"base_path": str(file)}
+    context = {"base_path": file_with_no_extension}
 
     show_details(context)
-
+    file_type = (
+        "directory" if os.path.isdir(file_with_no_extension) else "file"
+    )
     captured = capsys.readouterr()
     expected_output = (
         f"File name: {file_name}\n"
         f"File size in bytes: {size}\n"
-        f"File type: {'directory' if os.path.isdir(str(file)) else 'file'}\n"
+        f"File type: {file_type}\n"
         f"File extension: {file_extension or '[no extension]'}\n"
         f"Last modified date: {last_modified}\n"
     )
