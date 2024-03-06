50d4af8e5d893438ff2febc97d6d587246ff80b6
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-03-06 15:46:48 -0300

    test: function find_duplicate with no existing files

--- tests/actions/test_find_duplicate_files.py
+++ tests/actions/test_find_duplicate_files.py
@@ -1,1 +1,8 @@
+import pytest
 from pro_filer.actions.main_actions import find_duplicate_files  # NOQA
+
+
+def test_find_duplicate_no_existing_file(base_context):
+    with pytest.raises(ValueError) as excinfo:
+        find_duplicate_files(base_context)
+    assert "All files must exist" in str(excinfo.value)
