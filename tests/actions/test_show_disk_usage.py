b1a79a398996bdaf667f42df62e8557d52505857
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-03-06 14:39:55 -0300

    test: function show_disk_usage.py

--- tests/actions/test_show_disk_usage.py
+++ tests/actions/test_show_disk_usage.py
@@ -1,1 +1,50 @@
+import os
+from unittest.mock import patch
 from pro_filer.actions.main_actions import show_disk_usage  # NOQA
+
+import pytest
+
+
+@pytest.fixture
+def mock_context(existing_file, file_with_no_extension):
+    return {
+        "all_files": [
+            existing_file,
+            file_with_no_extension,
+        ]
+    }
+
+
+def test_show_disk_usage(capsys, mock_context):
+    with patch(
+        "pro_filer.actions.main_actions._get_printable_file_path"
+    ) as mock_get_printable_file_path:
+        # Configuração do mock para _get_printable_file_path
+        mock_get_printable_file_path.return_value = "file.txt".ljust(70)
+
+        # Chama a função que será testada
+        show_disk_usage(mock_context)
+
+        # Captura a saída do teste
+        captured = capsys.readouterr()
+
+        # Calcula o total_size esperado usando a mesma
+        # lógica que a função original
+
+        mock_sum = sum(
+            os.path.getsize(file) for file in mock_context["all_files"]
+        )
+        expected_output = ""
+        for file_path in sorted(
+            mock_context["all_files"], key=os.path.getsize, reverse=True
+        ):
+            file_size = os.path.getsize(file_path)
+            expected_output += (
+                f"'{mock_get_printable_file_path.return_value}':"
+                f" {file_size} ({int(file_size / mock_sum * 100)}%)\n"
+            )
+        expected_output = (
+            expected_output + "Total size: " + str(mock_sum) + "\n"
+        )
+
+        assert captured.out == expected_output
